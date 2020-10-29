9e09055654762206522f5fdc487e85dc78590357
Sulekha S <suls@cisco.com>
2020-10-29 22:19:45 +0530

    Add images and template + html changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,11 @@ export class AppComponent implements OnInit {
   mainPlaylistId = '37i9dQZF1DX6p4TJxzMRDe';
   mainPlaylistData: any;
 
+  SOTDData: any;
+  SOTDAlbumData: any;
+
+  oneDAlbums: any;
+
   genres: any;
 
   constructor(private http: HttpClient) {}
@@ -41,9 +46,10 @@ export class AppComponent implements OnInit {
     // console.log(token);
     this.getOneDirectionInfo(token);
     this.getMainPlaylist(token);
+    this.getOneDirectionAlbums(token);
   }
 
-   requestToken() {
+  requestToken() {
     const url = 'https://accounts.spotify.com/api/token';
     const httpOptions = {
       headers: new HttpHeaders({
@@ -59,6 +65,7 @@ export class AppComponent implements OnInit {
   async getOneDirectionInfo(token: String) {
     console.log('Getting 1d artist info');
     const oneD = await this.getArtist(this.oneDArtistId, token);
+    console.log(oneD);
     this.oneDData = {};
     this.oneDData.name = oneD['name'];
     this.oneDData.spotifyLink = oneD['external_urls']['spotify'];
@@ -69,14 +76,63 @@ export class AppComponent implements OnInit {
     console.log(this.oneDData);
   }
 
+  async getOneDirectionAlbums(token: String) {
+    console.log('Getting 1d albums');
+    const oneDAlbums = await this.getAlbums(this.oneDArtistId, token);
+    this.oneDAlbums = oneDAlbums['items'].slice(0, 5);
+    console.log(this.oneDAlbums);
+
+
+  }
+
   async getMainPlaylist(token) {
     this.mainPlaylistData = await this.getPlayList(this.mainPlaylistId, token);
     console.log('So are we displaying the main playlist? ');
     console.log(this.mainPlaylistData);
     const link = this.mainPlaylistData['external_urls']['spotify'];
+    const tracks = this.mainPlaylistData['tracks'];
+    const songoftheday = this.getSongOfTheDay(tracks);
     console.log(link);
   }
 
+  getSongOfTheDay(tracks) {
+    const randomTrackNumber = this.getRandomTrackNumber(tracks.items.length);
+    console.log('The track for the day is at' + randomTrackNumber);
+    console.log(tracks.items[randomTrackNumber].track);
+
+    this.SOTDData = tracks.items[randomTrackNumber].track;
+    this.SOTDAlbumData = this.SOTDData.album;
+
+    const SOTDPreviewUrl = this.SOTDData.preview_url;
+    const music = new Audio(SOTDPreviewUrl);
+    const sotdimgcard = document.getElementById('sotdImageDiv');
+    const sotdimage = document.getElementById('sotdImage');
+    const sotdplayOverlay = document.getElementById('sotdPlayOverlay');
+    sotdimgcard.addEventListener('mouseover', function () {
+      sotdimgcard.style.cursor = 'pointer';
+      sotdimage.style.filter = 'brightness(0.5)';
+      sotdplayOverlay.style.opacity = '1';
+      music.play();
+    }, false);
+    sotdimgcard.addEventListener('mouseout', function () {
+      music.pause();
+      sotdimage.style.filter = 'brightness(1)';
+      sotdplayOverlay.style.opacity = '0';
+    }, false);
+    console.log(this.SOTDAlbumData);
+  }
+
+  getRandomTrackNumber(length) {
+    const now = new Date();
+    const start = new Date(now.getFullYear(), 0, 0);
+    const diff = (now.getTime() - start.getTime()) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+    const oneDay = 1000 * 60 * 60 * 24;
+    const day = Math.floor(diff / oneDay);
+    console.log('Day of year: ' + day);
+
+    const randomTrackNum = day % length;
+    return randomTrackNum;
+  }
 
   getPlayList(playlistId: String, token: any) {
     const url = 'https://api.spotify.com/v1/playlists/' + playlistId;
@@ -109,36 +165,47 @@ export class AppComponent implements OnInit {
 
   }
 
+  getAlbums(artistId: String, token: any) {
+    const url = 'https://api.spotify.com/v1/artists/' + artistId + '/albums';
+    // console.log(token);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer ' + token
+      })
+    };
+    const artistReq = this.http.get(url, httpOptions);
+    artistReq.subscribe((artistdata) => {
+      // console.log(artistdata);
+    });
+    return artistReq.toPromise();
+
+  }
+
+  getAlbumById(albumId: String, token: any) {
+    const url = 'https://api.spotify.com/v1/albums/' + albumId;
+    // console.log(token);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer ' + token
+      })
+    };
+    const albumReq = this.http.get(url, httpOptions);
+    albumReq.subscribe((albumdata) => {
+      // console.log(artistdata);
+    });
+    return albumReq.toPromise();
+
+  }
+
+
   async getCleanedUpArtistInfo(artistId, token) {
     const artist = await this.getArtist(artistId, token);
     const artistData = {};
   }
 
-  /*getToken = async () => {
 
-    const result = await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/x-www-form-urlencoded',
-            'Authorization' : 'Basic ' + btoa(this.clientId + ':' + this.clientSecret)
-        },
-        body: 'grant_type=client_credentials'
-    });
-    const data = await result.json();
-    console.log(data);
-    return data['access_token'];
-}
 
-getGenres = async (token: string) => {
-  console.log('So is the token getting passed here?');
-  console.log(token);
-  const result = await fetch('https://api.spotify.com/v1/playlists/37i9dQZF1DX6p4TJxzMRDe/tracks', {
-      method: 'GET',
-      headers: { 'Authorization' : 'Bearer ' + token}
-  });
-
-  const data = await result.json();
-  return data.categories.items;
-}*/
 
 }
